3fc9e1522aa19de51caab1e900c3c7b6b2ba1cce
Pragaash Mohan <pragaashm@gmail.com>
2021-04-07 13:48:06 +0200

    small change

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from mfrc522 import SimpleMFRC522
 from time import sleep
 
 
-GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setwarnings(False)    
 GPIO.setmode(GPIO.BOARD)
 
 
@@ -18,13 +18,20 @@ while True:
             "print(text)"
             sleep(1)
 
-            if id==473653218634:
+            if id==904913957185:
                 sleep(1)
                 print('Hei Pragaash Mohan')
                 sleep(1)
                 print('Du er kul')
                 sleep(1)
                 print('Ta en pullup')
+                sleep(1)
+            
+            elif id == 926582318801:
+                sleep(1)
+                print('Hei Andreas Glomsrud')
+                sleep(1)
+                print('Last ned Tinder')
                 sleep(1)
 
             else:
