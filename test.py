097282a89699aa425848c4426216fcff7b15b29b
pragaashm <80519963+pragaashm@users.noreply.github.com>
2021-04-07 13:01:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from time import sleep
-led=40
+
+
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led,GPIO.OUT)
@@ -16,11 +17,11 @@ while True:
             print(text)
             sleep(2)
 
-            if id==872589727:
-                sleep(2)
-                GPIO.output(led, GPIO.HIGH)
-                sleep(1)
+            if id==473652318634:
+                print('Hei Pragaash Mohan')
+                print('Du er kul')
+                print('Ta en pullup')
+
             else:
-                GPIO.output(led, GPIO.LOW)
         except:
                 GPIO.cleanup()
