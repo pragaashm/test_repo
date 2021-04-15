27dc8a6875a32fa2e984d58af232809a61ba37ae
pragaashm <80519963+pragaashm@users.noreply.github.com>
2021-04-15 10:33:55 +0200

    Add files via upload

---
+++ read.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import keyboard
+
+from mfrc522 import SimpleMFRC522
+from time import sleep
+
+
+GPIO.setwarnings(False)    
+reader = SimpleMFRC522()
+
+rfid_Y = True;
+
+while rfid_Y:
+        try:
+            
+            
+            id, text = reader.read()
+            print(id)
+            print(type(id))
+
+
+            keyboard.send(str(id))
+            sleep(1)
+
+        except:
+                GPIO.cleanup()
